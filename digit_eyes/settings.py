cd55fe4c60568fdfc4da57331161b918f9732698
Raven Redstain <raven@mx>
2021-03-24 18:29:37 +0500

    added rotaing proxies and changed the code to not make login requests

--- digit_eyes/settings.py
+++ digit_eyes/settings.py
@@ -19,10 +19,10 @@ ROBOTSTXT_OBEY = False
 CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
-DOWNLOAD_DELAY = 2
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 32
-# CONCURRENT_REQUESTS_PER_IP = 1
+# CONCURRENT_REQUESTS_PER_DOMAIN = 32
+CONCURRENT_REQUESTS_PER_IP = 1
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'
 
@@ -37,21 +37,6 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 
 #    'digit_eyes.middlewares.DigitEyesSpiderMiddleware': 543,
 # }
 
-# Enable or disable downloader middlewares
-# DOWNLOADER_MIDDLEWARES = {
-#     'digit_eyes.middlewares.DigitEyesDownloaderMiddleware': None,
-#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
-# }
-# RANDOM_UA_TYPE = 'desktop.firefox'
-# RANDOM_UA_SAME_OS_FAMILY = True
-# RANDOM_UA_PER_PROXY = True
-# FAKEUSERAGENT_FALLBACK = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'
-
-# Enable or disable extensions
-# EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-# }
 
 # Configure item pipelines
 ITEM_PIPELINES = {
@@ -72,3 +57,47 @@ MEDIA_ALLOW_REDIRECTS = True
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
+
+# Enable or disable downloader middlewares
+########## scrapy-useragent #################
+# DOWNLOADER_MIDDLEWARES = {
+#     'digit_eyes.middlewares.DigitEyesDownloaderMiddleware': None,
+#     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+#     'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+# }
+# RANDOM_UA_TYPE = 'desktop.firefox'
+# RANDOM_UA_SAME_OS_FAMILY = True
+# RANDOM_UA_PER_PROXY = True
+# FAKEUSERAGENT_FALLBACK = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0'
+
+# Enable or disable extensions
+# EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+# }
+############ scrapy-proxies ##################
+# Retry many times since proxies often fail
+RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+    'scrapy_proxies.RandomProxy': 100,
+    'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+}
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+PROXY_LIST = 'proxies.txt'
+
+# Proxy mode
+# 0 = Every requests have different proxy
+# 1 = Take only one proxy from the list and assign it to every requests
+# 2 = Put a custom proxy to use in the settings
+PROXY_MODE = 0
+
+# If proxy mode is 2 uncomment this sentence :
+#CUSTOM_PROXY = "http://host1:port"
--- digit_eyes/spiders/digiteyes.py
+++ digit_eyes/spiders/digiteyes.py
@@ -19,90 +19,92 @@ class DigiteyesSpiderSpider(scrapy.Spider):
     # allowed_domains = ['http://www.digit-eyes.com']
     api_url = "https://www.digit-eyes.com/cgi-bin/digiteyes.cgi/"
     barcodes = pd.read_excel(io="Barcodes.xlsx")['barcode']
-    logins_df = pd.read_csv("logins.csv")
-    REQ_PER_LOGIN = 450
+    # logins_df = pd.read_csv("logins.csv")
+    # REQ_PER_LOGIN = 450
 
     def __init__(self, sku_range="15-19", *args, **kwargs):
         super(DigiteyesSpiderSpider, self).__init__(*args, **kwargs)
-        dispatcher.connect(self.spider_closed, signals.spider_closed)
+        # dispatcher.connect(self.spider_closed, signals.spider_closed)
         self.start_index = int(sku_range.split('-')[0])
         self.end_index = int(sku_range.split('-')[1])
         assert self.start_index < self.end_index, "start of range should be less than end"
 
-    def spider_closed(self, spider):
-        """
-        Save the logins file with additional info about usage.
-        """
-        self.logger.debug("Saving modified logins file to logins.csv")
-        self.logins_df.to_csv("logins.csv", index=False)
+    # def spider_closed(self, spider):
+    #     """
+    #     Save the logins file with additional info about usage.
+    #     """
+    #     self.logger.debug("Saving modified logins file to logins.csv")
+    #     self.logins_df.to_csv("logins.csv", index=False)
 
-    def start_requests(self):
-        """
-        Setup sessions for multiple accounts and call create account method
-        """
-        num_items = self.end_index - self.start_index
-        login_bins = num_items // self.REQ_PER_LOGIN
-        remaining_reqs = num_items - (login_bins*self.REQ_PER_LOGIN)
-        if remaining_reqs > 0:
-            login_bins += 1
+    # def start_requests(self):
+    #     """
+    #     Setup sessions for multiple accounts and call create account method
+    #     """
+    #     num_items = self.end_index - self.start_index
+    #     login_bins = num_items // self.REQ_PER_LOGIN
+    #     remaining_reqs = num_items - (login_bins*self.REQ_PER_LOGIN)
+    #     if remaining_reqs > 0:
+    #         login_bins += 1
 
-        for i in range(login_bins):
-            temp_start = self.start_index + (self.REQ_PER_LOGIN-1)*(i)
-            temp_end = self.start_index + (self.REQ_PER_LOGIN-1)*(i+1)
-            if temp_end > self.end_index:
-                # plus one to make the last item range inclusive
-                temp_end = self.end_index + 1
-            item_range = str(temp_start)+'-'+str(temp_end)
+    #     for i in range(login_bins):
+    #         temp_start = self.start_index + (self.REQ_PER_LOGIN-1)*(i)
+    #         temp_end = self.start_index + (self.REQ_PER_LOGIN-1)*(i+1)
+    #         if temp_end > self.end_index:
+    #             # plus one to make the last item range inclusive
+    #             temp_end = self.end_index + 1
+    #         item_range = str(temp_start)+'-'+str(temp_end)
 
-            yield Request(self.api_url,
-                          callback=self.create_account,
-                          dont_filter=True,
-                          meta={"cookiejar": i,
-                                "sku_range": item_range,
-                                "login_account_num": i})
+    #         yield Request(self.api_url,
+    #                       callback=self.create_account,
+    #                       dont_filter=True,
+    #                       meta={"cookiejar": i,
+    #                             "sku_range": item_range,
+    #                             "login_account_num": i})
 
-    def create_account(self, response):
-        """
-        This method will create an account, if not existed, 
-        otherwise account will be logged in.
-        """
-        login_account_num = response.meta["login_account_num"]
-        self.logins_df.loc[login_account_num,
-                           "status"] = response.meta["sku_range"]
-        self.logins_df.loc[login_account_num, "dated"] = time.asctime()
-        form_data = {
-            "upcCode": "",
-            "realName": self.logins_df.loc[login_account_num, "name"],
-            "email": self.logins_df.loc[login_account_num, "email"],
-            "workPhone": "",
-            "passwd": "%A&U$gn=7L",
-            "passwd2": "%A&U$gn=7L",
-            "question": "Select+a+verification+question",
-            "response": "",
-            "address": "",
-            "address2": "",
-            "city": "",
-            "state": "",
-            "otherstate": "",
-            "zip": "",
-            "country": "US",
-            "apiUser": "2487",
-            "action": "Create+Account"
-        }
-        print(self.logins_df.loc[login_account_num])
-        return FormRequest.from_response(response,
-                                         formdata=form_data,
-                                         callback=self.generate_item_urls,
-                                         meta={"cookiejar": response.meta["cookiejar"],
-                                               "sku_range": response.meta["sku_range"]})
+    # def create_account(self, response):
+    #     """
+    #     This method will create an account, if not existed,
+    #     otherwise account will be logged in.
+    #     """
+    #     login_account_num = response.meta["login_account_num"]
+    #     self.logins_df.loc[login_account_num,
+    #                        "status"] = response.meta["sku_range"]
+    #     self.logins_df.loc[login_account_num, "dated"] = time.asctime()
+    #     form_data = {
+    #         "upcCode": "",
+    #         "realName": self.logins_df.loc[login_account_num, "name"],
+    #         "email": self.logins_df.loc[login_account_num, "email"],
+    #         "workPhone": "",
+    #         "passwd": "%A&U$gn=7L",
+    #         "passwd2": "%A&U$gn=7L",
+    #         "question": "Select+a+verification+question",
+    #         "response": "",
+    #         "address": "",
+    #         "address2": "",
+    #         "city": "",
+    #         "state": "",
+    #         "otherstate": "",
+    #         "zip": "",
+    #         "country": "US",
+    #         "apiUser": "2487",
+    #         "action": "Create+Account"
+    #     }
+    #     # self.logger.debug("cookiejar is " + str(response.meta["cookiejar"]))
+    #     # self.logger.debug("sku_range is " + response.meta["sku_range"])
+    #     return FormRequest.from_response(response,
+    #                                      formdata=form_data,
+    #                                      callback=self.generate_item_urls,
+    #                                      meta={"cookiejar": response.meta["cookiejar"],
+    #                                            "sku_range": response.meta["sku_range"]})
 
-    def generate_item_urls(self, response):
-        # print(response.request.url)
+    def start_requests(self, response):
         # open_in_browser(response)
 
         urls = []
-        start = response.meta["sku_range"].split("-")[0]
-        end = response.meta["sku_range"].split("-")[-1]
+        # start = response.meta["sku_range"].split("-")[0]
+        # end = response.meta["sku_range"].split("-")[-1]
+        start = self.start_index
+        end = self.end_index
         barcodes_slice = self.barcodes.iloc[int(start): int(end)]
 
         for b in barcodes_slice:
@@ -112,6 +114,8 @@ class DigiteyesSpiderSpider(scrapy.Spider):
         # for url in urls:
         #     yield scrapy.Request(url=url, callback=self.parse_item,
         #                          meta={"cookiejar": response.meta["cookiejar"]})
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_item)
 
     def parse_item(self, response):
         item = DigitEyesItem()
